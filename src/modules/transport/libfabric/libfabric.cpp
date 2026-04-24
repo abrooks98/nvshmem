411a1a8119240fb4bcece508719a8f8b98aed4a2
Eric Raut <eraut@amazon.com>
2026-04-24 15:04:35 -0700

    libfabric: Add sequence counter wrapup/wrapdown helpers
    The wrapdown helper will be used in the next commit
    
    Signed-off-by: Eric Raut <eraut@amazon.com>
    (cherry picked from commit cefe723240ddd8cb269fe6ad3e1761d02f1249d7)
    Signed-off-by: Alex Brooks <albrooks@nvidia.com>

--- src/modules/transport/libfabric/libfabric.cpp
+++ src/modules/transport/libfabric/libfabric.cpp
@@ -930,12 +930,6 @@ static int nvshmemt_libfabric_put_signal_completion(nvshmem_transport_t transpor
         uint32_t &next_seq = signal_state.next_expected_seq[pe];
 
         while (true) {
-            // Skip reserved sequence number
-            if (next_seq == NVSHMEM_STAGED_AMO_SEQ_NUM) {
-                next_seq = (next_seq + 1) & nvshmemt_libfabric_endpoint_seq_counter_t::sequence_mask;
-                continue;
-            }
-
             auto *it = completion_map->find(next_seq);
 
             if (!it) break;
@@ -987,7 +981,7 @@ static int nvshmemt_libfabric_put_signal_completion(nvshmem_transport_t transpor
             }
 
             completion_map->erase(next_seq);
-            next_seq = (next_seq + 1) & nvshmemt_libfabric_endpoint_seq_counter_t::sequence_mask;
+            next_seq = nvshmemt_libfabric_endpoint_seq_counter_t::seq_num_wrapup(next_seq + 1);
         }
     }
 
