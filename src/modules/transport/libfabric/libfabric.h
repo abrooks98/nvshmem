amazon-contributing/upstream-to-nvshmem

transport/libfabric: replace unordered_map with std::vector for signal ordering
Replace three std::unordered_map instances in the signal ordering path:

1. proxy_put_signal_comp_map: unordered_map<uint64_t, comp_entry_t> replaced
   with std::vector<signal_seq_map> indexed by PE. Each signal_seq_map uses
   a 64-slot flat array with O(1) direct indexing by (seq % 64), falling back
   to std::unordered_map on slot collision.

2. put_signal_seq_counter_per_pe: unordered_map<int, seq_counter_t> replaced
   with std::vector<seq_counter_t> initialized to n_pes in connect_endpoints.

3. next_expected_seq: unordered_map<int, uint32_t> replaced with
   std::vector<uint32_t> initialized to n_pes in connect_endpoints.

src/modules/transport/libfabric/libfabric.h
@@ -7,6 +7,7 @@
 #include <assert.h>
 #include <stdint.h>  // IWYU pragma: keep
 #include <stdio.h>
+#include <cstdlib>
 #include <stddef.h>
 #include <string.h>
 #include <atomic>
@@ -22,6 +23,7 @@
 // IWYU pragma: no_include <bits/stdint-uintn.h>
 
 #include "non_abi/nvshmem_build_options.h"
+#include "non_abi/nvshmemx_error.h"
 #include "device_host_transport/nvshmem_common_transport.h"
 #include "internal/host_transport/nvshmemi_transport_defines.h"
 
@@ -510,6 +512,45 @@ class threadSafeOpQueue {
     }
 };
 
+/**
+ * Per-PE flat array + overflow map for signal completion entries.
+ * Fast path: O(1) direct index by (seq % WINDOW). Covers normal operation.
+ * Slow path: falls back to std::unordered_map on slot collision (different seq
+ * maps to same slot). This only happens if a single PE has >WINDOW concurrent
+ * unmatched entries.
+ */
+struct signal_seq_map {
+    static constexpr int WINDOW = 64;
+    struct slot { nvshmemt_libfabric_comp_entry_t entry; uint32_t seq; bool occupied; };
+    slot slots[WINDOW] = {};
+    std::unordered_map<uint32_t, nvshmemt_libfabric_comp_entry_t> overflow;
+
+    nvshmemt_libfabric_comp_entry_t *find(uint32_t seq) {
+        slot *s = &slots[seq % WINDOW];
+        if (s->occupied && s->seq == seq) return &s->entry;
+        auto it = overflow.find(seq);
+        return (it != overflow.end()) ? &it->second : nullptr;
+    }
+
+    void insert(uint32_t seq, const nvshmemt_libfabric_comp_entry_t &e) {
+        slot *s = &slots[seq % WINDOW];
+        if (!s->occupied) {
+            s->entry = e; s->seq = seq; s->occupied = true;
+        } else if (s->seq != seq) {
+            overflow[seq] = e;
+        } else {
+            NVSHMEMI_WARN_PRINT("signal_seq_map: duplicate insert for seq=%u", seq);
+            assert(false && "signal_seq_map: duplicate insert");
+        }
+    }
+
+    void erase(uint32_t seq) {
+        slot *s = &slots[seq % WINDOW];
+        if (s->occupied && s->seq == seq) s->occupied = false;
+        else overflow.erase(seq);
+    }
+};
+
 /*
  * Each index of the vectors contain a domain-specific resource. Host domain resources are first,
  * proceeded by proxy domain resources. The number of each domain type is specified by
@@ -521,9 +562,9 @@ class threadSafeOpQueue {
  * of an endpoint is stored directly in nvshmemt_libfabric_endpoint_t (domain_index).
  */
 typedef struct {
-    std::unordered_map<int, nvshmemt_libfabric_endpoint_seq_counter_t> *put_signal_seq_counter_per_pe;
-    std::unordered_map<uint64_t, nvshmemt_libfabric_comp_entry_t> *proxy_put_signal_comp_map;
-    std::unordered_map<int, uint32_t> *next_expected_seq;
+    std::vector<nvshmemt_libfabric_endpoint_seq_counter_t> put_signal_seq_counter_per_pe;
+    std::vector<signal_seq_map> proxy_put_signal_comp_map;
+    std::vector<uint32_t> next_expected_seq;
 } nvshmemt_libfabric_signal_state_t;
 
 typedef struct {

abrooks98/nvshmem

(cherry picked from commit 4adb5f3f97e768893931791182c69844a190b22e)
Signed-off-by: Alex Brooks <albrooks@nvidia.com>

src/modules/transport/libfabric/libfabric.h
@@ -7,6 +7,7 @@
 #include <assert.h>
 #include <stdint.h>  // IWYU pragma: keep
 #include <stdio.h>
+#include <cstdlib>
 #include <stddef.h>
 #include <string.h>
 #include <atomic>
@@ -23,6 +24,7 @@
 // IWYU pragma: no_include <bits/stdint-uintn.h>
 
 #include "non_abi/nvshmem_build_options.h"
+#include "non_abi/nvshmemx_error.h"
 #include "device_host_transport/nvshmem_common_transport.h"
 #include "internal/host_transport/nvshmemi_transport_defines.h"
 
@@ -294,10 +296,58 @@ struct nvshmemt_libfabric_comp_entry_t {
     };
 };
 
+/**
+ * Per-PE flat array + overflow map for signal completion entries.
+ * Fast path: O(1) direct index by (seq % window). Covers normal operation.
+ * Slow path: falls back to std::unordered_map on slot collision.
+ */
+struct signal_seq_map {
+    static constexpr size_t window = 64;
+
+    struct slot {
+        nvshmemt_libfabric_comp_entry_t entry;
+        uint32_t seq;
+        bool occupied;
+    };
+
+    std::array<slot, window> slots{};
+    std::unordered_map<uint32_t, nvshmemt_libfabric_comp_entry_t> overflow;
+
+    nvshmemt_libfabric_comp_entry_t *find(uint32_t seq) {
+        slot &s = slots[seq % window];
+        if (s.occupied && s.seq == seq) return &s.entry;
+        auto it = overflow.find(seq);
+        return (it != overflow.end()) ? &it->second : nullptr;
+    }
+
+    void insert(uint32_t seq, const nvshmemt_libfabric_comp_entry_t &e) {
+        slot &s = slots[seq % window];
+        if (!s.occupied) {
+            s.entry = e;
+            s.seq = seq;
+            s.occupied = true;
+        } else if (s.seq != seq) {
+            overflow[seq] = e;
+        } else {
+            NVSHMEMI_WARN_PRINT("signal_seq_map: duplicate insert for seq=%u", seq);
+            assert(false && "signal_seq_map: duplicate insert");
+        }
+    }
+
+    void erase(uint32_t seq) {
+        slot &s = slots[seq % window];
+        if (s.occupied && s.seq == seq) {
+            s.occupied = false;
+        } else {
+            overflow.erase(seq);
+        }
+    }
+};
+
 struct nvshmemt_libfabric_signal_state_t {
-    std::unordered_map<int, nvshmemt_libfabric_endpoint_seq_counter_t> put_signal_seq_counter;
-    std::unordered_map<uint64_t, nvshmemt_libfabric_comp_entry_t> proxy_put_signal_comp_map;
-    std::unordered_map<int, uint32_t> next_expected_seq;
+    std::vector<nvshmemt_libfabric_endpoint_seq_counter_t> put_signal_seq_counter;
+    std::vector<signal_seq_map> proxy_put_signal_comp_map;
+    std::vector<uint32_t> next_expected_seq;
 
     void clear() {
         put_signal_seq_counter.clear();